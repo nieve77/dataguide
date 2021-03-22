14b18832addccd59bef48aaa9de14bfd22b352f8
nieve77 <nieve1126@gmail.com>
2021-03-23 07:41:03 +0900

    Add files via upload
    c# 개발

---
+++ dataguide_project/dataguide_project/dataguide_project/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace dataguide_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Save = new DevExpress.XtraEditors.SimpleButton();
+            this.txtFileName = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.progressBarControl1 = new DevExpress.XtraEditors.ProgressBarControl();
+            this.btnUPLD_EXL = new DevExpress.XtraEditors.SimpleButton();
+            this.listBoxControl1 = new DevExpress.XtraEditors.ListBoxControl();
+            this.btn_Clear = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFileName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(12, 57);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(179, 60);
+            this.btn_Save.TabIndex = 1;
+            this.btn_Save.Text = "프로그램 시작";
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click_1);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(92, 30);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(320, 20);
+            this.txtFileName.TabIndex = 2;
+            this.txtFileName.EditValueChanged += new System.EventHandler(this.txtFileName_EditValueChanged);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(27, 13);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(0, 14);
+            this.labelControl1.TabIndex = 3;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(12, 13);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(179, 14);
+            this.labelControl2.TabIndex = 4;
+            this.labelControl2.Text = "데이터가이드 엑셀 데이터 import";
+            // 
+            // progressBarControl1
+            // 
+            this.progressBarControl1.Location = new System.Drawing.Point(12, 121);
+            this.progressBarControl1.Name = "progressBarControl1";
+            this.progressBarControl1.Size = new System.Drawing.Size(438, 18);
+            this.progressBarControl1.TabIndex = 6;
+            this.progressBarControl1.EditValueChanged += new System.EventHandler(this.progressBarControl1_EditValueChanged);
+            // 
+            // btnUPLD_EXL
+            // 
+            this.btnUPLD_EXL.Location = new System.Drawing.Point(13, 29);
+            this.btnUPLD_EXL.Name = "btnUPLD_EXL";
+            this.btnUPLD_EXL.Size = new System.Drawing.Size(75, 22);
+            this.btnUPLD_EXL.TabIndex = 7;
+            this.btnUPLD_EXL.Text = "파일 업로드";
+            this.btnUPLD_EXL.Click += new System.EventHandler(this.btnUPLD_EXL_Click);
+            // 
+            // listBoxControl1
+            // 
+            this.listBoxControl1.Location = new System.Drawing.Point(12, 146);
+            this.listBoxControl1.Name = "listBoxControl1";
+            this.listBoxControl1.Size = new System.Drawing.Size(438, 190);
+            this.listBoxControl1.TabIndex = 8;
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(197, 57);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(100, 60);
+            this.btn_Clear.TabIndex = 9;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 348);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.listBoxControl1);
+            this.Controls.Add(this.btnUPLD_EXL);
+            this.Controls.Add(this.progressBarControl1);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btn_Save);
+            this.Name = "Form1";
+            this.Text = "Dataguide_Import";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtFileName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btn_Save;
+        private DevExpress.XtraEditors.TextEdit txtFileName;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.ProgressBarControl progressBarControl1;
+        private DevExpress.XtraEditors.SimpleButton btnUPLD_EXL;
+        private DevExpress.XtraEditors.ListBoxControl listBoxControl1;
+        private DevExpress.XtraEditors.SimpleButton btn_Clear;
+    }
+}
+
---
+++ dataguide_project/dataguide_project/dataguide_project/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using DevExpress.XtraEditors.Controls;
+using Excel = Microsoft.Office.Interop.Excel;
+using SHDocVw;
+using mshtml;
+using System.Diagnostics;
+
+
+
+namespace dataguide_project
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch sw = new Stopwatch();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void btn_Save_Click_1(object sender, EventArgs e)
+        {
+            //Process.Start(@"C:\\Program Files\\FnGuide\DataGuide\\FnDGLoader.exe");
+            listBoxControl1.Items.Add("데이터가이드 실행파일을 여는 중입니다.");
+            
+            progressBarControl1.Properties.Step = 5;
+            progressBarControl1.Properties.PercentView = true;
+            progressBarControl1.Properties.Minimum = 10;
+            progressBarControl1.Properties.Maximum = 100;
+           
+                
+
+            Excel.Application excelApp = null;
+            Excel.Workbook wb = null;
+
+            try
+            {
+                excelApp = new Excel.Application();  
+                sw.Start();
+
+                listBoxControl1.Items.Add(DateTime.Now.ToString("yyyy-MM-dd")+ " 엑셀파일을 여는 중입니다.");
+                wb = excelApp.Workbooks.Open(txtFileName.Text);
+
+                excelApp.Application.Run("DateChange");
+                while (progressBarControl1.Position < progressBarControl1.Properties.Maximum)
+                {
+                    progressBarControl1.PerformStep();
+                    progressBarControl1.Update();
+                }
+
+                wb.SaveCopyAs(@"\\10.0.1.48\자료실\기업가치평가실_평가거시지표\DG\" + DateTime.Now.ToString("yyyyMMddhhMMss") + "_데이터가이드.xlsm");
+                wb.Close(true);
+                excelApp.Quit();
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ReleaseExcelObject(wb);
+                ReleaseExcelObject(excelApp);
+                listBoxControl1.Items.Add("데이터가이드 정보 저장완료 했습니다.");
+                sw.Stop();
+                listBoxControl1.Items.Add((sw.ElapsedMilliseconds / 1000).ToString() + "초 걸렸습니다");
+                sw.Reset();
+
+            }
+        }
+
+        static void ReleaseExcelObject(object obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    Marshal.ReleaseComObject(obj);
+                    obj = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                throw ex;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void progressBarControl1_EditValueChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void btnUPLD_EXL_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pFileDlg = new OpenFileDialog();
+            pFileDlg.Filter = "excel files(*.xlsm)|*.xlsm|엑셀파일(*.xlsx)|*.xlsx|All Files(*.*)|*.*";
+            pFileDlg.Title = "데이터가이드 엑셀파일을 선택해 주세요.";
+            if (pFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                 txtFileName.Text = pFileDlg.FileName;
+
+            }
+        }
+
+        private void txtFileName_EditValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+                progressBarControl1.EditValue = 0;
+                listBoxControl1.Items.Clear();
+        }
+
+    }
+}
